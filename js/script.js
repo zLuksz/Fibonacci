ba02af7a88ba9503a5fe83ab24356b5a49320c8f
Lucas Souza Santos <lucas.souza_santos@live.com>
2023-06-14 21:29:17 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,39 +1,18 @@
-function fibonacciteste() {
-    let i = 1;
-    let pen = 0;
-    let ult = 1;
+function exibirFibonacci() {
+    var penultimo = 0;
+    var ultimo = 1;
+    var fibonacci = penultimo + ultimo;
+    var fibonacciOutput = document.getElementById("fibonacciOutput");
+    fibonacciOutput.innerHTML = "1";
 
-    while (i <= 500) {
-        if (i === ult + pen) {
-            document.getElementById("resultado").innerHTML = (i)
-            console.log(i);
-            pen = ult;
-            ult = i;
+    var interval = setInterval(function() {
+        if (fibonacci > 500) {
+            clearInterval(interval);
+            return;
         }
-    i++;
-    }
+        fibonacciOutput.innerHTML += " - " + fibonacci;
+        penultimo = ultimo;
+        ultimo = fibonacci;
+        fibonacci = penultimo + ultimo;
+    }, 500);
 }
-
-function fibonacci(n) {
-    let fibSeq = []
-    let pen = 0, ult = 1
-
-    while (fibSeq.length < n) {
-        fibSeq.push(pen)
-        let temp = pen
-        pen = ult
-        ult = temp + pen
-    }
-
-    return fibSeq;
-}
-
-let fibonacciSequence = fibonacci(15);
-
-let htmlList = "<ul>"
-fibonacciSequence.forEach(function(num) {
-    htmlList += "<li>" + num + "</li>"
-})
-htmlList += "</ul>"
-
-document.getElementById("resultado").innerHTML = htmlList
